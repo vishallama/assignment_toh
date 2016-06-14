2fd9dc98ceffef8b410e6c029c68b9640213cd68
Vishal Lama <vishal.lama@gmail.com>
2016-06-14 07:29:51 -0600

    Add #TowerOfHanoi#instructions
    The method returns a string containing the instructions for the game.

--- towers.rb
+++ towers.rb
@@ -3,4 +3,12 @@ class TowerOfHanoi
     "# Welcome to Tower of Hanoi!"
   end
 
+  def instructions
+    <<~HEREDOC
+      # Instructions:
+      # Enter where you would like to move from and to
+      # in the format [1, 3]. Enter 'q' to quit.
+    HEREDOC
+  end
+
 end
